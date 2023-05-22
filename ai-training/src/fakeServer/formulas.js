e2f58b37e03d98f02781d9bc08ffb55825de3f25
RandyIp <randyip9@mail.com>
2023-05-22 14:45:38 -0700

    Finished Covariance function

--- ai-training/src/fakeServer/formulas.js
+++ ai-training/src/fakeServer/formulas.js
@@ -3,19 +3,21 @@ import data from './db.json'
 const formulas = {
   // write covariance formula assumes matrix is array of arrays
   // X and Y are the number of the columns you want to get covariance of
+  // dataset = [ [data data data], [data data data]] each array would be a column
   'covFunc': (X, Y, dataSet) => {
     var Xsum = 0;
     var Ysum = 0;
-    for (let i = 0; i < dataSet[0].length; i++) {
-      Xsum += dataSet[X][i]
-      Ysum += dataSet[Y][i]
+    for (let i = 0; i < dataSet.length; i++) {
+      Xsum += dataSet[i][X]
+      Ysum += dataSet[i][Y]
     }
-    const Xmean = Xsum / dataSet[0].length
-    const Ymean = Ysum / dataSet[0].length
+    const Xmean = Xsum / dataSet.length
+    const Ymean = Ysum / dataSet.length
     var numerator = 0
-    for (let i = 0; i < dataSet[0].length; i++) {
-
+    for (let i = 0; i < dataSet.length; i++) {
+      numerator += (dataSet[i][X] - Xmean) * (dataSet[i][Y] - Ymean)
     }
+    return numerator / (dataSet.length - 1)
   },
   // covariance matrix formula
   'covMat': (X) => {
