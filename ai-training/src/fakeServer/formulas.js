3189bbfbd745f4acba48ef35cc7913c14e77e31a
RandyIp <randyip9@mail.com>
2023-05-22 13:39:32 -0700

    Updated ML formulas

--- ai-training/src/fakeServer/formulas.js
+++ ai-training/src/fakeServer/formulas.js
@@ -1,9 +1,21 @@
 import data from './db.json'
 
 const formulas = {
-  // write covariance formula
-  'covFunc': (X, Y) => {
+  // write covariance formula assumes matrix is array of arrays
+  // X and Y are the number of the columns you want to get covariance of
+  'covFunc': (X, Y, dataSet) => {
+    var Xsum = 0;
+    var Ysum = 0;
+    for (let i = 0; i < dataSet[0].length; i++) {
+      Xsum += dataSet[X][i]
+      Ysum += dataSet[Y][i]
+    }
+    const Xmean = Xsum / dataSet[0].length
+    const Ymean = Ysum / dataSet[0].length
+    var numerator = 0
+    for (let i = 0; i < dataSet[0].length; i++) {
 
+    }
   },
   // covariance matrix formula
   'covMat': (X) => {
