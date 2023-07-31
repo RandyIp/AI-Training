9f58869102e9a079435395e57ee8cdb080c8b7bd
RandyIp <randyip9@mail.com>
2023-07-30 23:20:56 -0700

    Added Routes

--- ai-training/src/App.js
+++ ai-training/src/App.js
@@ -1,52 +1,20 @@
-import './App.css';
-import styled from 'styled-components';
-import { useState, useRef } from 'react';
-import Homepage from './components/homepage.js'
-import Navbar from './components/navbar.js'
-import Menu from './components/menu.js'
-import FlappybirdAI from './components/FlappybirdAI.js';
-import NeuralNetworkVisual from './components/NeuralNetworkVisual.js'
-import Jarvis from './components/jarvis.js'
-
-function App() {
-  const [page, setPage] = useState('Home')
-  const [scroll, setScroll] = useState()
-  const HomeRef = useRef(); const ProjectRef = useRef(); const AboutRef = useRef();
-
-  // useEffect(() => {
-  if (scroll === 'Home') {
-    HomeRef.current.scrollIntoView()
-    setScroll()
-  }
-  if (scroll === 'Project') {
-    ProjectRef.current.scrollIntoView()
-    setScroll()
-  }
-  if (scroll === 'About') {
-    AboutRef.current.scrollIntoView()
-    setScroll()
-  }
-  // }, [scroll])
-
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from './components/home.js'
+const App = () => {
   return (
-    <Container>
-      <Navbar page={page} setPage={setPage} setScroll={setScroll} />
-      {page === 'Menu' && <Menu
-        HomeRef={HomeRef} ProjectRef={ProjectRef} AboutRef={AboutRef}
-        setPage={setPage}
-        setScroll={setScroll}
-      />}
-      {page === 'Home' && <Homepage
-        HomeRef={HomeRef} ProjectRef={ProjectRef} AboutRef={AboutRef} setPage={setPage}
-      />}
-      {page === 'project1' && <NeuralNetworkVisual />}
-      {page === 'project2' && <Jarvis />}
-      {page === 'project3' && <FlappybirdAI />}
-    </Container>
+    <Router>
+      <Routes>
+        <Route path='/mlvis' element={<Home initialPage='project1' />} />
+        <Route path='/jarvis' element={<Home initialPage='project2' />} />
+        <Route path="/" element={<Home initialPage='Home' />} />
+      </Routes>
+    </Router >
   );
 }
 
-const Container = styled.div`
-background-color:black;
-`
 export default App;
---
+++ ai-training/src/components/home.js
@@ -0,0 +1,49 @@
+import styled from 'styled-components';
+import { useState, useRef } from 'react';
+import Homepage from './homepage.js'
+import Navbar from './navbar.js'
+import Menu from './menu.js'
+import FlappybirdAI from './FlappybirdAI.js';
+import NeuralNetworkVisual from './NeuralNetworkVisual.js'
+import Jarvis from './jarvis.js'
+
+const Home = ({ initialPage }) => {
+  const [page, setPage] = useState(initialPage)
+  const [scroll, setScroll] = useState()
+  const HomeRef = useRef(); const ProjectRef = useRef(); const AboutRef = useRef();
+
+  if (scroll === 'Home') {
+    HomeRef.current.scrollIntoView()
+    setScroll()
+  }
+  if (scroll === 'Project') {
+    ProjectRef.current.scrollIntoView()
+    setScroll()
+  }
+  if (scroll === 'About') {
+    AboutRef.current.scrollIntoView()
+    setScroll()
+  }
+
+  return (
+    <Container>
+      <Navbar page={page} setPage={setPage} setScroll={setScroll} />
+      {page === 'Menu' && <Menu
+        HomeRef={HomeRef} ProjectRef={ProjectRef} AboutRef={AboutRef}
+        setPage={setPage}
+        setScroll={setScroll}
+      />}
+      {page === 'Home' && <Homepage
+        HomeRef={HomeRef} ProjectRef={ProjectRef} AboutRef={AboutRef} setPage={setPage}
+      />}
+      {page === 'project1' && <NeuralNetworkVisual />}
+      {page === 'project2' && <Jarvis />}
+      {page === 'project3' && <FlappybirdAI />}
+    </Container>
+  );
+}
+
+const Container = styled.div`
+background-color:black;
+`
+export default Home
